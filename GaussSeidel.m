e14b8494859bfea80a042b1b7c92877912e61f21
Henry R. Da Silva <henryqx@hotmail.com>
2017-04-11 14:05:24 -0300

    Gauss Seidel

--- GaussSeidel.m
+++ GaussSeidel.m
@@ -1,15 +1,19 @@
-function [x, iters] = GaussJacobi(A, b, prec)
+
+function [x, iters] = GaussSeidel(A, b, prec)
   [r, c] = size(A);
   y = zeros(r, 1);
   x = zeros(r, 1);
   e = 1;
   iters = 0;
-
+  
   while e > prec
     for i=1: r
       soma = 0;    
       for j=1: r
-        if (j!=i)
+        if (j < i)
+          soma += A(i, j) * x(j);
+        end 
+        if (j > i)
           soma += A(i, j) * y(j);
         end
       end
