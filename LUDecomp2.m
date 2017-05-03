0a12eb58ccdc99ec8780b0009b929888ac1991dc
Henry Rodrigues Da Silva <henryqx@hotmail.com>
2017-05-02 21:13:14 -0300

    LU decomposition with only one matrix

---
+++ LUDecomp2.m
@@ -0,0 +1,43 @@
+% LU decomposition using only one matrix
+function x = LUDecomp2(A, B)
+  [r, c] = size(A);
+  LU = zeros(r, c);
+  for k = 1 : r
+    for i = k : r
+      z = k - 1;
+      row = LU(i, 1 : z);
+      col = LU(1 : z, k);
+      LU(i, k) = A(i, k) - (row * col);
+      if (i > k)
+        row = LU(k, 1 : z);
+        col = LU(1 : z, i);
+        LU(k, i) = (A(k, i) - (row * col)) / LU(k, k);
+      end
+    end
+  end
+  y = RetroSubInferior(LU, B);
+  x = RetroSubSuperior(LU, y);
+end
+
+function x = RetroSubSuperior(A, b)
+  n = size(A)(1);
+  x = zeros(1, n);
+  x(n) = b(n);
+  for i = n - 1 : -1 : 1
+    j =  i + 1;
+    soma = sum(A(i, j : n) .* x(j : n));
+    x(i) = b(i) - soma;
+  end
+end
+
+function y = RetroSubInferior(A, b)
+  n = size(A);
+  y = zeros(1, n);
+  y(1) = b(1) / A(1, 1);
+  for i = 2 : n
+    j = i - 1;
+    soma = sum(A(i, 1 : j) .* y(1 : j));
+    y(i) = (b(i) - soma) / A(i, i);
+  end
+end
+
--- test.m
+++ test.m
@@ -1,3 +1,14 @@
-f = @(x) exp(x).*sin(x) -1;
-%[x, iter] = Bissecao(f,0, 1, 1e-6)
-[x, iter] = FalsaPosicao(f,0, 1, 1e-6)
+C=[ 4  2  3;
+    2 -4 -1;
+   -1  1 4
+  ];
+B = [7; 
+     1; 
+    -5];
+
+X1 = LUDecomp2(C, B)
+X2 = LUDecomp(C, B)
+
+%f = @(x) exp(x).*sin(x) -1;
+%df = @(x) exp(x) .* (sin(x) .+ cos(x));
+%[x2, iter2] = Newton(f,df, 1, 1e-6)
