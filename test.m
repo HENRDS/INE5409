b4befdbca7edcdcd9ffa86c2e1c770b7478d18e1
Henry Rodrigues Da Silva <henryqx@hotmail.com>
2017-05-04 15:02:11 -0300

    Secant method

--- test.m
+++ test.m
@@ -1,14 +1,5 @@
-C=[ 4  2  3;
-    2 -4 -1;
-   -1  1 4
-  ];
-B = [7; 
-     1; 
-    -5];
+f = @(x) exp(x).*sin(x) -1;
+df = @(x) exp(x) .* (sin(x) .+ cos(x));
 
-X1 = LUDecomp2(C, B)
-X2 = LUDecomp(C, B)
-
-%f = @(x) exp(x).*sin(x) -1;
-%df = @(x) exp(x) .* (sin(x) .+ cos(x));
-%[x2, iter2] = Newton(f,df, 1, 1e-6)
+%[x2, iter2] = Newton(f,df, 0, 1e-6)
+[x, iter] = Secant(f,0, 1, 1e-6)
