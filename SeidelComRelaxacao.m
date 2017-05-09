e4d3a6c9ea178521563242f7c986896ee467ace1
Henry Rodrigues Da Silva <henryqx@hotmail.com>
2017-05-09 18:18:54 -0300

    Seidel with relaxation

---
+++ SeidelComRelaxacao.m
@@ -0,0 +1,22 @@
+function [x, iters] = SeidelComRelaxacao(prec, A, b, w)
+  n = size(A)(2);
+  y = zeros(n, 1);
+  x = zeros(n, 1);
+  e = 1;
+  iters = 0;
+
+  while e > prec
+    for i = 1 : n
+      soma = 0;
+      for j = 1 : n
+        if j ~= i
+            soma += A(i, j) * x(j);
+        end
+      end
+      x(i) = (1 - w) * y(i) + w * ((b(i) - soma) / A(i, i));
+    end
+    e = max(abs(x - y));
+    y = x;
+    iters += 1;
+  end
+end
