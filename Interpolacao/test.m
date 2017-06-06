c11765e85c442768fe2bc171e5099f971dd48b9a
Henry Rodrigues Da Silva <henryqx@hotmail.com>
2017-06-06 14:59:36 -0300

    Division differences added

--- Interpolacao/test.m
+++ Interpolacao/test.m
@@ -1,8 +1,10 @@
-px = [-1 0 2];
-py = [4 1 -1];
-x =[-1:.01:2];
-plot(x, Lagrange(x, px, py));
-grid
+px = [-3 -2 1 3];
+py = [-1 2 -1 10 ];
+x =[0:12];
+yy = spline(px, py, x)
+plot(x, yy)
+grid;
 hold on
-% plot(x, Poli(Polinomial(px, py),x), 'r')
-% hold on
+y = DivDiffs(x, px, py)
+plot(x, y, 'r')
+hold on
